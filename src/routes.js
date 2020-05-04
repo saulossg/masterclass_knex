975bb8f1f870a1ca950159ecade393d94fa3129d
Saulo Gomes <saulo.gomes@tbfg.com.br>
2020-05-04 01:54:02 -0300

    Created and config update and delete

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const routes = express.Router();
 const UserController = require('./controllers/UserController');
 
-routes.post('/users', UserController.create);
 routes.get('/users', UserController.index);
+routes.post('/users', UserController.create);
+routes.put('/users/:id', UserController.update);
+routes.delete('/users/:id', UserController.delete);
 
 module.exports = routes;
